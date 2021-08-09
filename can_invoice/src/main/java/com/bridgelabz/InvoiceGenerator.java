6457c302541bd6b6e7b92b5814a216e5b702305d
vivek <vivek.iitd93@gmail.com>
2021-08-09 23:55:19 +0530

    to calculate fare multiple rides

--- can_invoice/src/main/java/com/bridgelabz/InvoiceGenerator.java
+++ can_invoice/src/main/java/com/bridgelabz/InvoiceGenerator.java
@@ -4,8 +4,8 @@ public class InvoiceGenerator {
 
     /**
      * Given : MINIMUM_COST_PER_KM = 10.0
-     *         COST_PER_TIME = 1
-     *         MINIMUM_FARE = 5
+     * COST_PER_TIME = 1
+     * MINIMUM_FARE = 5
      */
     private static final double MINIMUM_COST_PER_KM = 10.0;
     private static final int COST_PER_TIME = 1;
@@ -13,8 +13,8 @@ public class InvoiceGenerator {
 
     /**
      * Purpose : Given distance and time,
-     *           Return total fare for the journey
-     *
+     * Return total fare for the journey
+     * <p>
      * Condition : If minimum total fare is less than the MINIMUM_FARE, return MINIMUM_FARE
      *
      * @param distance
@@ -24,5 +24,20 @@ public class InvoiceGenerator {
         double totalFare = distance * MINIMUM_COST_PER_KM + time * COST_PER_TIME;
 
         return Math.max(totalFare, MINIMUM_FARE);
+    }
+
+    /**
+     * Purpose : Given distance and time for multiple rides,
+     * Return aggregate total fare for all the journey
+     *
+     * @param rides
+     * @return
+     */
+    public double calculateTotalFare(Ride[] rides) {
+        double totalFare = 0.0;
+        for (Ride ride : rides) {
+            totalFare += this.calculateFare(ride.distance, ride.time);
+        }
+        return totalFare;
     }
 }
